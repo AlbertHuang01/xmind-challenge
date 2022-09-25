ef86356f3dac3ab8111550f042059b4825fe82c6
huangzhichang <1092824484@qq.com>
2022-09-25 11:51:27 +0800

    添加快照测试及基本配置

---
+++ src/main.test.tsx
@@ -0,0 +1,12 @@
+import {render} from '@testing-library/react'
+import { App } from './main'
+
+describe('main.tsx',()=>{
+  // 主入口快照不需要记录
+  // test('snapshot',()=>{
+  //   const {baseElement}=render(<App/>)
+  //   expect(baseElement).toMatchSnapshot()
+  // })
+
+})
+
