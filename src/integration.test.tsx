f5139348b197f534efb9568cadb8139afee4570b
AlbertHuang01 <1092824484@qq.com>
2022-10-13 19:46:12 +0800

    新增一个条件筛选的集成测试

--- src/integration.test.tsx
+++ src/integration.test.tsx
@@ -1,21 +1,26 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen, act, waitFor } from '@testing-library/react';
 import dayjs from 'dayjs';
 import { BILL_TYPE } from './contant';
 import { Bill } from './model';
 import * as service from './service';
 import ReactDOM from 'react-dom';
 import { App } from '.';
+import { renderHook } from '@testing-library/react-hooks';
+import { useAppContext } from './context';
 
 jest.mock('./service')
+jest.useFakeTimers()
 
 const time = dayjs('2020-01-01')
+// 填充一条当前日期的数据，为了能够再后续的测试中被查询到
+const now = dayjs()
 const mockBillList: Bill[] = [
   { time: time.valueOf(), type: BILL_TYPE.INCOME, amount: 1, category: '1' },
-  { time: time.valueOf(), type: BILL_TYPE.EXPENDITURE, amount: 2, category: '2' },
+  { time: now.valueOf(), type: BILL_TYPE.EXPENDITURE, amount: 2, category: '2' },
 ]
 
 describe('App', () => {
-  beforeAll(()=>{
+  beforeAll(() => {
     jest.spyOn(service, 'queryBillList').mockResolvedValue(Promise.resolve(mockBillList))
     jest.spyOn(service, 'queryCategoryList').mockResolvedValue(Promise.resolve([
       { id: '1', name: '1', type: BILL_TYPE.INCOME },
@@ -23,28 +28,53 @@ describe('App', () => {
     ]))
   })
 
-  // it('should condition change',()=>{
-  //   const { baseElement } = render(<App />)
-  //   const monthsEl = baseElement.querySelector('[id="months"]')
-  //   fireEvent.change(monthsEl, { target: { value: '2000-01' } })
-  // })
+  const fireKeyDownOnElement = (selector: string) => {
+    let targetEl = document.querySelector(selector)
+    fireEvent.keyDown(targetEl)
+  }
 
-  // it('shoud months filter', (done) => {
-  //   const { baseElement } = render(<App />)
-  //   let antTableRows = null
-  //   process.nextTick(() => {
-  //     antTableRows = baseElement.querySelectorAll('.ant-table-tbody .ant-table-row')
-  //     expect(antTableRows).toHaveLength(2)
-  //   })
+  const getAntTableRows = () => {
+    return document.querySelectorAll('.ant-table-tbody .ant-table-row')
+  }
 
-  //   const monthsEl = baseElement.querySelector('[id="months"]')
-  //   fireEvent.change(monthsEl, { target: { value: '2000-01' } })
-  //   process.nextTick(() => {
-  //     antTableRows = baseElement.querySelectorAll('.ant-table-tbody .ant-table-row')
-  //     expect(antTableRows).toHaveLength(0)
-  //     done()
-  //   })
-  // })
+  /**
+   * 判断月筛选是否生效
+   * 1. 首先不筛选应该有两条数据
+   * 2. 筛选后，条件不对应该没有数据
+   * 3. 筛选条件正确，应该有一条数据
+   */
+  it('shoud months filter', async () => {
+    render(<App />)
+    expect.hasAssertions()
+
+    // filter 1
+    await waitFor(() => {
+      expect(getAntTableRows()).toHaveLength(2)
+    })
+
+    // filter 2
+    fireKeyDownOnElement('[id="months"]')
+
+    let targetMonth = document.querySelector('[title="2022-01"]')
+    fireEvent.click(targetMonth)
+
+    await waitFor(() => {
+      expect(getAntTableRows()).toHaveLength(0)
+    })
+
+    // filter 3
+    fireKeyDownOnElement('[id="months"]')
+
+    const year = now.year()
+    const month = now.month() + 1
+    targetMonth = document.querySelector(`[title="${year}-${month}"]`)
+    fireEvent.click(targetMonth)
+
+    await waitFor(() => {
+      expect(getAntTableRows()).toHaveLength(1)
+    })
+
+  })
 
 })
 
